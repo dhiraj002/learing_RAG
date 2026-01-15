2a16be416e821353424e8359389164296ff5b3c1
Dhiraj Dhiman <dhirajdhiman15@gmail.com>
2026-01-15 22:42:02 +0530

    refactor: update package.json name and dependencies; implement indexTheDoc function in prepare.js

--- prepare.js
+++ prepare.js
@@ -1,3 +1,18 @@
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
 
-function indexTheDoc(filePath) {}
+export async function indexTheDoc(filePath) {
+  const loader = new PDFLoader(filePath, { splitPages: false });
+  const docs = await loader.load();
+
+  const splitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 500,
+    chunkOverlap: 100,
+  });
+  const texts = await splitter.splitText(docs[0].pageContent);
+  console.log(texts.length);
+
+  //
+}
+
+// indexTheDoc("MERN_jd.pdf");
